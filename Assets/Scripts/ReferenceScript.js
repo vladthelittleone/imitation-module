46dc0766693556e8426ee9350dca7bc1d186cf67
vaimer <overespada@gmail.com>
2015-06-01 21:41:51 +0300

    new label and new table

--- Assets/Scripts/ReferenceScript.js
+++ Assets/Scripts/ReferenceScript.js
@@ -2,16 +2,18 @@
 
 public var BUTTON_HEIGHT:int = 500;
 public var BUTTON_WIDTH:int = 500;
+public var skin: GUISkin;
 
 function OnGUI()
 {	
+	GUI.skin = skin;
 	var textBX: int = Screen.width / 2 + 450;
 	var textBY: int =  Screen.height / 2 + 200;
 	if(GUI.Button(
 		new Rect(
 		textBX,
 		textBY,
-		60,
+		100,
 		60
 		),"Назад"
 	))
